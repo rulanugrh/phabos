93abb6d5d87d333b27b535704e5ce6a1f42635ff
rulanugrh <nextarituenak01@gmail.com>
2024-08-31 00:54:04 +0700

    chore: edit for handling jwt error

--- src/middleware/verify.ts
+++ src/middleware/verify.ts
@@ -1,69 +1,108 @@
-import jwt, {JsonWebTokenError, TokenExpiredError} from 'jsonwebtoken';
-import { NextFunction, Request, Response } from 'express';
+import jwt, { JsonWebTokenError, JwtPayload, TokenExpiredError} from 'jsonwebtoken'
+import { NextFunction, Request, Response } from 'express'
 
+export interface CustomRequest extends Request {
+    token: string | JwtPayload
+}
 export const verify = async(req: Request, res: Response, next: NextFunction) => {
     const token = req.headers.authorization as string
+    try {    
+        if (token === null) {
+            return res.status(401).json({
+                msg: 'sorry you must login first'
+            })
+        }
+        
+        const decoded = jwt.verify(token, process.env.APP_SECRET as string);
+        (req as CustomRequest).token = decoded;
+        next()
+    } catch (err) {
+        if (err instanceof JsonWebTokenError) {
 
-    if (token === null) {
-        return res.status(401).json({
-            msg: 'sorry you must login first'
+            if (err.message === "jwt malformed") {
+                return res.status(403).json({
+                    code: 403,
+                    msg: 'Invalid Token, you are want to bypass this site?'
+                })
+            } else if (err.message === "invalid signature") {
+                return res.status(403).json({
+                    code: 403,
+                    msg: 'invalid signature or secret token'
+                })
+            } else {
+                return res.status(400).json({
+                    code: 400,
+                    msg: err.message
+                })
+            }
+        }
+        if ( err instanceof TokenExpiredError) {
+            return res.status(400).json({
+                code: 400,
+                err: err.message
+            })
+        }
+
+        return res.status(400).json({
+            code: 400,
+            msg: err
         })
     }
-
-    jwt.verify(token, process.env.APP_SECRET as string, (err: any, email: any) => {
-        if (err instanceof JsonWebTokenError) {
-            return res.status(400).json({
-                message: err.message,
-                code: 400,
-            })
-        }
-
-        if ( err instanceof TokenExpiredError) {
-            res.status(400).json({
-                message: err.message,
-            })
-        }
-
-        req.body.email = email
-        next()
-    })
 }
 
 export const siteAdmin = async(req: Request, res: Response, next: NextFunction) => {
     const token = req.headers.authorization as string
-
-    if (token === null) {
-        return res.status(401).json({
-            msg: 'sorry you must login first'
-        })
-    }
-
-    const { email } = jwt.verify(token, process.env.APP_SECRET as string) as {
-        email: string
-    }
-
-    if (email !== process.env.ADMIN_EMAIL) {
-        return res.status(403).json({
-            code: 403,
-            msg: 'this page forbidden, youre role not support'
-        })
-    }
-    
-    jwt.verify(token, process.env.APP_SECRET as string, (err: any, email: any) => {
-        if (err instanceof JsonWebTokenError) {
-            return res.status(400).json({
-                message: err.message,
-                code: 400,
+    try {
+        if (token === null) {
+            return res.status(401).json({
+                msg: 'sorry you must login first'
             })
+        }
+    
+        const { email } = jwt.verify(token, process.env.APP_SECRET as string) as {
+            email: string
+        }
+    
+        if (email !== process.env.ADMIN_EMAIL) {
+            return res.status(403).json({
+                code: 403,
+                msg: 'this page forbidden, youre role not support'
+            })
+        }
+        
+        const decoded = jwt.verify(token, process.env.APP_SECRET as string);
+        (req as CustomRequest).token = decoded;
+        next()
+    } catch (err) {
+        if (err instanceof JsonWebTokenError) {
+            if (err.message === "jwt malformed") {
+                return res.status(403).json({
+                    code: 403,
+                    msg: 'Invalid Token, are you want to bypass this site?'
+                })
+            } else if (err.message === "invalid signature") {
+                return res.status(403).json({
+                    code: 403,
+                    msg: 'invalid signature or secret token'
+                })
+            }  else {
+                return res.status(400).json({
+                    code: 400,
+                    msg: err.message
+                })
+            }
         }
 
         if ( err instanceof TokenExpiredError) {
-            res.status(400).json({
-                message: err.message,
+            return res.status(400).json({
+                code: 400,
+                err: err.message
             })
         }
+        return res.status(400).json({
+            code: 400,
+            msg: err
+        })
+    }
 
-        req.body.email = email
-        next()
-    })
 }
