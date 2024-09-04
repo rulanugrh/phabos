e7f71d2cfcd90365d7318da7461de6ace1a7efab
rulanugrh <nextarituenak01@gmail.com>
2024-09-04 23:59:15 +0700

    feat: adding home routing

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Express } from "express"
+import express, { Express, Request, Response } from "express"
 import { Routes } from "./routes"
 import dotenv from 'dotenv';
 
@@ -8,6 +8,14 @@ const port = process.env.APP_PORT  ?? 3000
 
 async function main() {
     app.use(express.json())
+    
+    app.get('/', async (req: Request, res: Response): Promise<Response> => {
+        try {
+            return res.send("API Succesfully Running")
+        } catch (error) {
+            return res.send(String(error))
+        }
+    })
 
     Routes(app)
 
